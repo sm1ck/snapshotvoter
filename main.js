825ca838300d9af3b54e14781b781c48f680bd38
sm1ck <dosdosnope@gmail.com>
2022-11-21 21:26:15 +0500

    Add retry logic and try/catch

--- main.js
+++ main.js
@@ -15,35 +15,35 @@
  * node main.js <название_проекта> getprops
  */
 
-import ethers from 'ethers';
-import snapshot from '@snapshot-labs/snapshot.js';
-import * as accs from './accs.js';
-import fetch from 'node-fetch'
-import { exit } from 'process';
-import * as fs from 'fs';
-import * as path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// rpc node url
-
-const url = "https://rpc.ankr.com/eth";
-
-// Базовые переменные
-
-const rand_mode = 0; // 0 => стандартный, 1 => рандомная отправка варианта
-const random_min = 1; // минимальный номер в голосовании
-const random_max = 4; // максимальный номер в голосовании
-const isSleep = false; // задержка перед отправкой, нужна ли? изменить на true, если нужна
-const sleep_from = 30; // от 30 секунд
-const sleep_to = 60; // до 60 секунд
-const isPropList = false; // кастомный список проползалов
-let isParseProps = false;
-
+ import ethers from 'ethers';
+ import snapshot from '@snapshot-labs/snapshot.js';
+ import * as accs from './accs.js';
+ import fetch from 'node-fetch'
+ import { exit } from 'process';
+ import * as fs from 'fs';
+ import * as path from 'path';
+ import { fileURLToPath } from 'url';
+ 
+ const __filename = fileURLToPath(import.meta.url);
+ const __dirname = path.dirname(__filename);
+ 
+ // rpc node url
+ 
+ const url = "https://rpc.ankr.com/eth";
+ 
+ // Базовые переменные
+ 
+ const rand_mode = 0; // 0 => стандартный, 1 => рандомная отправка варианта
+ const random_min = 1; // минимальный номер в голосовании
+ const random_max = 3; // максимальный номер в голосовании
+ const isSleep = false; // задержка перед отправкой, нужна ли? изменить на true, если нужна
+ const sleep_from = 3; // от 3 секунд
+ const sleep_to = 5; // до 5 секунд
+ const isPropList = false; // кастомный список проползалов
+ let isParseProps = false;
+ 
 /**
- * Абстрактная задержка
+ * Абстрактная задержка (async)
  * @param {Integer} millis 
  * @returns 
  */
@@ -51,6 +51,14 @@ let isParseProps = false;
 const sleep = async (millis) => {
     return new Promise(resolve => setTimeout(resolve, millis));
 };
+
+/**
+ * Абстрактная задержка
+ * @param {Integer} millis 
+ * @returns 
+ */
+
+const wait = ms => new Promise(r => setTimeout(r, ms));
 
 /**
  * Случайное min/max целое значение
@@ -62,6 +70,102 @@ const sleep = async (millis) => {
 const randomIntInRange = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+/**
+ * Повтрная отправка действия
+ * @param {String} address адрес
+ * @param {Arrow function} operation стрелочная функция
+ * @param {Integer} delay задержка в милли секундах
+ * @param {Integer} retries количество повторов
+ * @returns Promise
+ */
+
+const retryOperation = (address, operation, delay, retries) => new Promise((resolve, reject) => {
+    return operation
+      .then(resolve)
+      .catch((reason) => {
+        if (retries > 0) {
+          console.log(`(Ошибка) ${address} => повторная отправка действия, задержка: ${delay}с, осталось попыток: ${retries - 1}`);
+          return wait(delay*1000)
+            .then(retryOperation.bind(null, address, operation, delay, retries - 1))
+            .then(resolve)
+            .catch(reject);
+        }
+        return reject(reason);
+    });
+});
+
+/**
+ * Голосование
+ * @param {Wallet} wallet 
+ * @param {String} address
+ * @param {String} prop
+ * @returns Promise
+ */
+
+const voteSnap = (ethWallet, address, prop) => new Promise((resolve, reject) => {
+    try {
+        const prom_vote = client.vote(ethWallet, address, {
+            space: project,
+            proposal: prop,
+            type: 'single-choice',
+            choice: rand_mode == 0 ? Number(vote) : randomIntInRange(random_min, random_max),
+            reason: '',
+            app: 'snapshot'
+        }).then((result) => {
+            if (result.hasOwnProperty('id')) {
+                console.log(`(Голосование) ${address} => голос засчитан`);
+            } else {
+                console.log(`(Голосование) ${address} =>`);
+                console.dir(result);
+            }
+            resolve();
+        }).catch((err) => {
+            console.log(`(Голосование) ${address} => ошибка "${err.error}": ${err.error_description}`);
+            if ((typeof err.error_description === 'string' && err.error_description.includes('many')) || typeof err.error_description !== 'string') {
+                reject();
+            } else {
+                resolve();
+            }
+        });
+    } catch (err) {
+        console.log(`(Голосование) ${address} => ошибка "${err.name}": ${err.message}`);
+        reject();
+    }
+});
+
+/**
+ * Подписка
+ * @param {Wallet} wallet 
+ * @param {String} address
+ * @returns Promise
+ */
+
+ const subSnap = (ethWallet, address) => new Promise((resolve, reject) => {
+    try {
+        const prom_sub = client.follow(ethWallet, address, {
+            space: project
+        }).then((result) => {
+            if (result.hasOwnProperty('id')) {
+                console.log(`(Подписка) ${address} => вы подписались`);
+            } else {
+                console.log(`(Подписка) ${address} =>`);
+                console.dir(result);
+            }
+            resolve();
+        }).catch((err) => {
+            console.log(`(Подписка) ${address} => ошибка "${err.error}": ${err.error_description}`);
+            if ((typeof err.error_description === 'string' && err.error_description.includes('many')) || typeof err.error_description !== 'string') {
+                reject();
+            } else {
+                resolve();
+            }
+        });
+    } catch (err) {
+        console.log(`(Подписка) ${address} => ошибка "${err.name}": ${err.message}`);
+        reject();
+    }
+});
 
 // Парсинг параметров
 
@@ -138,49 +242,14 @@ for (let acc of adata) {
 
         let prom_list = [];
         for (let prop of props_list) {
-            prom_list.push(new Promise((resolve, reject) => {
-                const prom_vote = client.vote(ethWallet, address, {
-                    space: project,
-                    proposal: prop,
-                    type: 'single-choice',
-                    choice: rand_mode == 0 ? Number(vote) : randomIntInRange(random_min, random_max),
-                    reason: '',
-                    app: 'snapshot'
-                }).then((result) => {
-                    if (result.hasOwnProperty('id')) {
-                        console.log('(Голосование) '+address+' => голос засчитан');
-                    } else {
-                        console.log('(Голосование) '+address+' =>');
-                        console.dir(result);
-                    }
-                    resolve();
-                }).catch((err) => {
-                    console.log('(Голосование) '+address+' => ошибка "'+err.error+'": '+err.error_description);
-                    resolve();
-                });
-            }));
+            prom_list.push(retryOperation(address, voteSnap(ethWallet, address, prop), isSleep ? randomIntInRange(sleep_from, sleep_to) : 1, 3));
         }
 
         // Подписка
 
-        prom_list.push(new Promise((resolve, reject) => {
-            const prom_sub = client.follow(ethWallet, address, {
-                space: project
-            }).then((result) => {
-                if (result.hasOwnProperty('id')) {
-                    console.log('(Подписка) '+address+' => вы подписались');
-                } else {
-                    console.log('(Подписка) '+address+' =>');
-                    console.dir(result);
-                }
-                resolve();
-            }).catch((err) => {
-                console.log('(Подписка) '+address+' => ошибка "'+err.error+'": '+err.error_description);
-                resolve();
-            });
-        }));
+        prom_list.push(retryOperation(address, subSnap(ethWallet, address), isSleep ? randomIntInRange(sleep_from, sleep_to) : 1, 3));
         
-        await Promise.all(prom_list).then(() => resolve());
+        await Promise.all(prom_list).then(() => resolve()).catch(() => resolve());
 
     });
     ++i;
@@ -189,7 +258,7 @@ for (let acc of adata) {
 
     if (isSleep) {
         let sle = randomIntInRange(sleep_from, sleep_to);
-        prom.then(() => i < adata.length ? console.log('Задержка '+sle+'с..') : null);
+        prom.then(() => i < adata.length ? console.log(`Задержка ${sle}с..`) : null);
         if (i < adata.length) {
             await sleep(sle * 1000);
         }  
