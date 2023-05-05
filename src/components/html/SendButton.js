920b11232fc775f5c0c4db85a37b85b50701ec1e
sm1ck <dosdosnope@gmail.com>
2023-05-05 15:59:35 +0500

    Remove .eth check validate in input & bump to 2.0.5

--- src/components/html/SendButton.js
+++ src/components/html/SendButton.js
@@ -2,53 +2,107 @@ import React, { useCallback } from "react";
 import { Panel, Button } from "react-bulma-components";
 import { ReadyState } from "react-use-websocket";
 import { INPUT_TYPE } from "shared";
-import { deepCheck, validateEth, validateProposal, validateIntegerWithHyphen } from "../../services/form";
+import {
+  deepCheck,
+  validateProposal,
+  validateIntegerWithHyphen,
+} from "../../services/form";
 import { useToast } from "../../contexts/ToastContext";
 import { useInputContext } from "../../contexts/InputContext";
 import { panelStyle } from "../styles/styles";
 
 export default function SendButton() {
-    const { isStarted, readyState, sendJsonMessage, setStarted } = useToast();
-    const { projectRef, propolsalRef, selectorInputRef, sleepRef, typeVotingRef, parsePropsRef } = useInputContext();
+  const { isStarted, readyState, sendJsonMessage, setStarted } = useToast();
+  const {
+    projectRef,
+    propolsalRef,
+    selectorInputRef,
+    sleepRef,
+    typeVotingRef,
+    parsePropsRef,
+  } = useInputContext();
 
-    // Отправляем данные на бекенд через веб-сокет
+  // Отправляем данные на бекенд через веб-сокет
 
-    const handleClickStartVote = useCallback(() => sendJsonMessage({
-        type: 'Vote',
+  const handleClickStartVote = useCallback(
+    () =>
+      sendJsonMessage({
+        type: "Vote",
         project: projectRef.current,
         propolsal: propolsalRef.current,
         vote: selectorInputRef.current,
         sleep: sleepRef.current,
         typeVote: typeVotingRef.current,
-        parseProps: parsePropsRef.current
-    }), [sendJsonMessage, projectRef, propolsalRef, selectorInputRef, sleepRef, typeVotingRef, parsePropsRef]);
+        parseProps: parsePropsRef.current,
+      }),
+    [
+      sendJsonMessage,
+      projectRef,
+      propolsalRef,
+      selectorInputRef,
+      sleepRef,
+      typeVotingRef,
+      parsePropsRef,
+    ]
+  );
 
-    const handleClickStopVote = useCallback(() => sendJsonMessage({ type: 'Stop' }), [sendJsonMessage]);
+  const handleClickStopVote = useCallback(
+    () => sendJsonMessage({ type: "Stop" }),
+    [sendJsonMessage]
+  );
 
-    // Валидация формы при отправке
+  // Валидация формы при отправке
 
-    const onVote = () => {
-        if (!deepCheck(INPUT_TYPE.PROJECT, validateEth(projectRef.current), 'Название должно заканчиваться на .eth')) {
-            return;
-        } else if (!parsePropsRef.current && !deepCheck(INPUT_TYPE.PROPOLSAL, validateProposal(propolsalRef.current), 'ID проползала должен начинаться с 0x и иметь длинну 66 символов')) {
-            return;
-        } else if (!deepCheck(INPUT_TYPE.SELECTOR, validateIntegerWithHyphen(selectorInputRef.current), 'Вариант должен быть целым числом')) {
-            return;
-        } else if (!deepCheck(INPUT_TYPE.SLEEP, validateIntegerWithHyphen(sleepRef.current), 'Значение задержки должно быть целым числом')) {
-            return;
-        }
-        setStarted(true);
-        handleClickStartVote();
-    };
+  const onVote = () => {
+    if (!deepCheck(INPUT_TYPE.PROJECT)) {
+      return;
+    } else if (
+      !parsePropsRef.current &&
+      !deepCheck(
+        INPUT_TYPE.PROPOLSAL,
+        validateProposal(propolsalRef.current),
+        "ID проползала должен начинаться с 0x и иметь длинну 66 символов"
+      )
+    ) {
+      return;
+    } else if (
+      !deepCheck(
+        INPUT_TYPE.SELECTOR,
+        validateIntegerWithHyphen(selectorInputRef.current),
+        "Вариант должен быть целым числом"
+      )
+    ) {
+      return;
+    } else if (
+      !deepCheck(
+        INPUT_TYPE.SLEEP,
+        validateIntegerWithHyphen(sleepRef.current),
+        "Значение задержки должно быть целым числом"
+      )
+    ) {
+      return;
+    }
+    setStarted(true);
+    handleClickStartVote();
+  };
 
-    const onStop = () => {
-        setStarted(false);
-        handleClickStopVote();
-    };
+  const onStop = () => {
+    setStarted(false);
+    handleClickStopVote();
+  };
 
-    return (
-        <Panel.Block style={panelStyle}>
-            <Button color="link" onClick={() => onVote()} disabled={readyState !== ReadyState.OPEN || isStarted}>Проголосовать</Button> <Button color="danger" onClick={() => onStop()} disabled={!isStarted}>Завершить</Button>
-        </Panel.Block>
-    );
-};
+  return (
+    <Panel.Block style={panelStyle}>
+      <Button
+        color="link"
+        onClick={() => onVote()}
+        disabled={readyState !== ReadyState.OPEN || isStarted}
+      >
+        Проголосовать
+      </Button>{" "}
+      <Button color="danger" onClick={() => onStop()} disabled={!isStarted}>
+        Завершить
+      </Button>
+    </Panel.Block>
+  );
+}
